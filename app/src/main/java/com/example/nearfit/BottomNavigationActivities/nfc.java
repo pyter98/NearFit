a7e93c82babad4cafc579790ce06519f879a5a34
Piero <pyter9@gmail.com>
2019-11-26 01:55:51 +0100

    2

--- app/src/main/java/com/example/nearfit/BottomNavigationActivities/nfc.java
+++ app/src/main/java/com/example/nearfit/BottomNavigationActivities/nfc.java
@@ -16,32 +16,15 @@ import androidx.lifecycle.Observer;
 import androidx.fragment.app.Fragment;
 import com.example.nearfit.BottomNavigationActivities.PostLoginActivity;
 
-import com.example.nearfit.MainActivity;
+
 import com.example.nearfit.R;
-import com.example.nearfit.SessionManager;
-import java.io.File;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.NfcAdapter.CreateNdefMessageCallback;
-import android.nfc.NfcEvent;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.nio.charset.Charset;
 
-import static android.nfc.NdefRecord.createMime;
 
 public class nfc extends Fragment {
     public String username;
     public String password;
-    NfcAdapter nfcAdapter;
-    TextView textView;
-    Intent intent;
     Button button;
     public nfc(String username, String password) {
         this.username = username;
@@ -63,8 +46,12 @@ public class nfc extends Fragment {
                 i.putExtra("username", username);
                 i.putExtra("password", password);
                 startActivity(i);
+                //getActivity().finish();
             }
         });
+
+        ((PostLoginActivity) getActivity()).setTextActionBar("Home Page");
+
 
         return root;
     }
